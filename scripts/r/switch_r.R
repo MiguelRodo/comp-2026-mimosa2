de3e79eedac562f9cd8f0f3242f3a7ae6c5987a1
Miguel Rodo <miguel.rodo@uct.ac.za>
2026-07-01 11:49:20 +0000

    Add switch_r

---
+++ scripts/r/switch_r.R
@@ -0,0 +1,31 @@
+
+switch_r <- function(wd) {
+  if (!dir.exists(wd)) {
+    stop("The specified directory does not exist.")
+  }
+
+  rm(list = ls(all.names = TRUE, envir = .GlobalEnv), envir = .GlobalEnv)
+
+  setwd(wd)
+
+  if (file.exists(".Renviron")) {
+    readRenviron(".Renviron")
+  }
+
+  if (file.exists(".Rprofile")) {
+    source(".Rprofile", local = .GlobalEnv)
+  }
+
+  if (file.exists(".RData")) {
+    load(".RData", envir = .GlobalEnv)
+  }
+  if (interactive() && file.exists(".Rhistory")) {
+    loadhistory(".Rhistory")
+  }
+
+  if (exists(".First", envir = .GlobalEnv, inherits = FALSE)) {
+    do.call(".First", list(), envir = .GlobalEnv)
+  }
+
+  invisible(NULL)
+}
